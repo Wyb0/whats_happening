36db902fe06110c1377f9cff68937d3b95b50f20
Andrew Wybo <andrewwybo@gmail.com>
2017-11-02 18:04:31 -0400

    getting html pull from scrape, working on url build

--- src/search.js
+++ src/search.js
@@ -2,22 +2,17 @@ const cheerio = require("cheerio")
 const request = require("request")
 
 //temp test case
-let search = "downtownorlando"
+let search = "unclelous"
 
 request("https://www.instagram.com/explore/tags/" + search, function (error, response, html) {
-
+  
     const $ = cheerio.load(html);
-
+console.log(html)
     const results = [];
-    let title = $("a._q8ysx").attr('href');
-
-    // $("a._q8ysx _6y8ij").each(function(i, element){
-
-    //     const href = $(element).attr("href");
-
-    //     results.push({
-    //         href: href
-    //     })
-    // })
-    console.log(title);
+    $("._70iju").each(function(i, element){
+        console.log("gflkjgkjlsdgfk;l")
+         const href = $(element).children[i].attr('href');
+         console.log(href);
+         results.push(href);
+    })
 })
